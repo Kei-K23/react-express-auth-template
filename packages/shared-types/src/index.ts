bf4f4f42ab5b9b6b7bf6565a287a55a6618a2609
Kei-K23 <keiksl2301@gmail.com>
2025-01-18 14:09:46 +0630

    init commit and setup monorep for ui, backend api and shared-type package

---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+// Schemas
+export const userSchema = z.object({
+  id: z.string().uuid(),
+  username: z
+    .string({ required_error: "Username is required" })
+    .trim()
+    .min(3, "Username should be at least 3 character"),
+  email: z.string({ required_error: "Email is required" }).email().trim(),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(6, "Password must be at least 6 characters long")
+    .max(18, "Password should not exceed 18 characters"),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
+});
+
+const userWithoutPasswordSchema = userSchema.omit({ password: true });
+
+export const registerSchema = z.object({
+  username: z
+    .string({ required_error: "Username is required" })
+    .trim()
+    .min(3, "Username should be at least 3 character"),
+  email: z.string({ required_error: "Email is required" }).email().trim(),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(6, "Password must be at least 6 characters long")
+    .max(18, "Password should not exceed 18 characters"),
+});
+
+export const loginSchema = z.object({
+  username: z
+    .string({ required_error: "Username is required" })
+    .trim()
+    .min(3, "Username should be at least 3 character"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(6, "Password must be at least 6 characters long")
+    .max(18, "Password should not exceed 18 characters"),
+});
+
+export const authResponseSchema = z.object({
+  token: z.string(),
+});
+
+// Types
+export type UserWithoutPasswordField = z.infer<
+  typeof userWithoutPasswordSchema
+>;
+export type User = z.infer<typeof userSchema>;
+export type LoginInput = z.infer<typeof loginSchema>;
+export type RegisterInput = z.infer<typeof registerSchema>;
+export type AuthResponse = z.infer<typeof authResponseSchema>;
