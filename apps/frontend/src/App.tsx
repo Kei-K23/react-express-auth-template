bbce8bc23fe6d25ad7dc2f52b4b34e1d06824554
Kei-K23 <keiksl2301@gmail.com>
2025-01-19 11:31:53 +0630

    feat-frontent: setup react-vite project with tailwind and shadcn UI

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-red-400">Hello</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
